5280d6d2b523688012501ccbb710f6e9571ece35
dmiedema <daniel@househappy.org>
2014-08-25 10:08:29 -0700

    Initial Commit

---
+++ HHAMessageCenter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HHAMessageCenter
+//
+//  Created by Daniel on 8/25/14.
+//  Copyright (c) 2014 Househappy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
